e656d01fa8c1ef84b88032bcfc635a3556e712f5
Desmond <desmondaldridge@gmail.com>
2020-10-25 02:00:37 -0500

    Seeded the database... possibly the funnest part of the assignment

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheddar Jalapeño Delux", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("KRABBY PATTY", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("ORGANIC NON-GMO GLUTEN FREE VEGGIE HEALTHBOMB", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Quadruple Pattie Longhorn Meat Stack", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Value Meal McDouble", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Syntho-bovine-flesh Labratory Mad Scientist Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Homemade with store bought ingredients", FALSE);
